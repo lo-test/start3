28e27d839af8ffd769e8bc4898d621430312a462
lo-test <dido_roy@hotmail.com>
2022-06-10 17:04:18 +0800

    working

--- src/index.js
+++ src/index.js
@@ -4,8 +4,3 @@ import '@popperjs/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 
-
-let ssqRedBall = document.createElement('div')
-ssqRedBall.innerHTML = 123
-
-document.querySelector('#ssq-redball').appendChild(ssqRedBall)
